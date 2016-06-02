5a07dd0818ccbb40c9b59a2e5e276b5f0d155607
Abdelrahman Hawwari (1000011934) <HAWWAG2@student.op.ac.nz>
2016-06-03 11:55:54 +1200

    Added Views and templates to create/edit posts inside the site and basic security.

--- mysite/urls.py
+++ mysite/urls.py
@@ -16,9 +16,10 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
-from django.conf.urls import include
+
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
     url(r'', include('blog.urls')),
 ]
+
